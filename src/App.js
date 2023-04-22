1444e9c1ac8b5ad6ef19d89ef27e7ee72e1e5f5a
hacı celal aygar <aygarcelal@gmail.com>
2023-04-22 03:57:27 +0300

    the commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Counter from './redux/counterSection/Counter';
 import { selectedCount } from './redux/counterSection/counterSlice';
 import { useSelector } from 'react-redux';
+import Authentication from './redux/authentication/Authentication';
 const App = () => {
 
   const count = useSelector(selectedCount);
   return (
     <div>
+      <Authentication />
+      <hr />
       <Counter />
       <br />
       <span>type : {count.type} </span>
--- src/redux/authentication/Authentication.js
+++ src/redux/authentication/Authentication.js
@@ -1,11 +1,37 @@
 
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Authentication = () => {
+    const [body, setBody] = useState({
+        username: "1",
+        password: "22",
+    });
+    const onChangeBody = e => {
+        let data = body;
+        data[e.target.name] = e.target.value;
+        setBody({ ...data });
+    }
+    const login = e => {
+        console.log("login")
+        console.log(body)
+    }
+    const logout = e => {
+        console.log("logout")
+        console.log(body)
+    }
     return (
         <div>
             <h5>Authentication</h5>
+            <hr />
+            <span>Username : </span>
+            <input type="text" name="username" value={body?.username} onChange={(e) => onChangeBody(e)} />
+            <br />
+            <span>Password : </span>
+            <input type="password" name="password" value={body?.password} onChange={(e) => onChangeBody(e)} />
+            <br />
+            <button type='submit' onClick={login}>Login</button>
+            <button type='submit' onClick={logout}>Logout</button>
         </div>
     );
 };
